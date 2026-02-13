f0bce3b0bee9171c7d1b5dc897d8f39d119a1db4
CRUZ MARTINEZ Jonas <jonas.cruz@soprasteria.com>
2026-02-13 14:50:04 +0100

    training subjects update

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 // server.js
 const express = require('express');
 const app = express();
+app.use(express.json());
 
-app.get('/', (_, res) => {
-  res.send('<h1>Test server is running!</h1>');
+app.get("/health", (req, res) => {
+  res.status(200).json({ status: "ok" });
 });
 
-app.listen(3000, () => console.log('Server running on http://localhost:3000'));
+app.listen(3000, () => console.log("Server running"));
